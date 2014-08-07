547e8f6fb927d568c5cf942c6adde792b61fff3e
Chris Leonard <lenwood.cc@gmail.com>
2014-08-07 00:15:53 -0500

    create plot 2

---
+++ plot2.R
@@ -0,0 +1,21 @@
+# read the data in
+classes <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", colClasses=classes, skip=66636, nrows=2880)
+names(data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+# convert date & time
+dateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+data["dateTime"] <- NA
+data$dateTime <- dateTime
+
+# clean up
+rm(classes, dateTime)
+data$Date <- NULL
+data$Time <- NULL
+data <- data[c("dateTime", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")]
+
+# create the line chart
+png(filename="plot2.png", height=480, width=480)
+par(mar=c(2, 4, 2, 2))
+plot(data$dateTime, data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)")
+dev.off()
